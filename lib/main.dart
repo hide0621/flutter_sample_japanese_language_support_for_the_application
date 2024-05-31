ade85f5089f5e42f03989047a94fb3db4baf04b4
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-31 09:28:01 +0900

    arbファイルとyamlファイルを用いたコード生成を使って、メッセージ（アプリ内で表示する独自の文字列）をローカライズしてみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MaterialApp(
-    localizationsDelegates: [
-      GlobalWidgetsLocalizations.delegate,
-      GlobalMaterialLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-    ],
-    supportedLocales: [
-      Locale('ja', 'JP'),
-    ],
+    // localizationsDelegates: [
+    //   GlobalWidgetsLocalizations.delegate,
+    //   GlobalMaterialLocalizations.delegate,
+    //   GlobalCupertinoLocalizations.delegate,
+    // ],
+    localizationsDelegates: L10n.localizationsDelegates,
+    // supportedLocales: [
+    //   Locale('ja', 'JP'),
+    // ],
+    supportedLocales: L10n.supportedLocales,
     home: HomeScreen(),
   ));
 }
@@ -21,11 +23,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// 以下のようにデフォルトロケールに固定値を設定するやり方もあるが、
-    /// これだと複数言語に対応することが出来ない...
-    // Intl.defaultLocale = 'ja';
-    /// なので以下の書き方が良い
     Intl.defaultLocale = Localizations.localeOf(context).toString();
+    final l10n = L10n.of(context);
     return Scaffold(
       body: Center(
         child: Column(
@@ -34,7 +33,8 @@ class HomeScreen extends StatelessWidget {
             const TextField(),
             Text(
               DateFormat.yMEd().format(DateTime.now()),
-            )
+            ),
+            Text(l10n.helloWorld),
           ],
         ),
       ),
